eef17371cc765044ca31cc7ee65fd54a7b395cc1
xujian <908634674@qq.com>
2020-12-17 22:43:01 +0800

    版本

--- html/js/public.js
+++ html/js/public.js
@@ -39,8 +39,8 @@ const { ipcRenderer } = require('electron');
         headers: {
           source: 'applet',
           userKey: userInfo.key || '',
-          version: '1.10.34',
-          Referer: 'https://servicewechat.com/wx6025c5470c3cb50c/223/page-frame.html',
+          version: '1.10.36',
+          Referer: 'https://servicewechat.com/wx6025c5470c3cb50c/225/page-frame.html',
           'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat'
         },
         key: requestKey
